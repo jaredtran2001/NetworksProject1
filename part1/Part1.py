31181b370d89d7afb11a889f95084cbd096a920e
j-cse <103292325+j-cse@users.noreply.github.com>
2023-04-16 03:43:16 -0700

    Part 1 STAGE a working
    STAGE b debug code added. Style changes so linter would stop yelling at me.

--- part1/Part1.py
+++ part1/Part1.py
@@ -1,9 +1,8 @@
 import socket
 import struct
 
-# define the server address and port number
-#SERVER_ADDRESS = 'attu2.cs.washington.edu'
-SERVER_ADDRESS = '127.0.0.1'
+SERVER_ADDRESS = 'attu2.cs.washington.edu'
+LOCALHOST = '127.0.0.1'
 PORT = 12235
 STEP1 = 1
 STEP2 = 2
@@ -12,78 +11,103 @@ HEADERSIZE = 12
 SERVERPACKAGESIZE = 16
 SERVERACKSIZE = 4
 
+
 def main():
-    p1StageA()
+    p1_stage_a()
 
-def p1StageA():
-    # Step a1 set up the UDP connection
-    socket_udp = createUDPConnection()
-    message = b'hello world'
-    # send data to the server
-    sendData = createHeaderandPackagePayload(message, 0, STEP1, DIGITS)
-    print("Sending packet for Stage A...")
+
+def p1_stage_a():
+    # create udp socket
+    socket_udp = create_udp_socket()
+    payload = b'hello world'
+
+    # package and send packet to the server
+    sendData = package_header_and_payload(payload, 0, STEP1, DIGITS)
+    print(f"\tSending UDP packet to {SERVER_ADDRESS} on port {PORT}...")
     socket_udp.sendto(sendData, (SERVER_ADDRESS, PORT))
-    print("Sent")
+    print("\tSent")
 
-    # Step a2
-    # receive data from the server
-    print("Receiving packet from server...")
-    receiveData, server_address = socket_udp.recvfrom(HEADERSIZE + SERVERPACKAGESIZE)
-    # Print out message
-    print("Received")
-    receivedBuffer = struct.unpack("!III I", receiveData[HEADERSIZE:])
-    secret = receivedBuffer[3]
-    print("Stage A secret: " + str(secret))
-    p1StageB(receivedBuffer, socket_udp)
+    # receive packet from the server
+    print(f"\tReceiving packet from {SERVER_ADDRESS}... ")
+    receiveData, server_address = socket_udp.recvfrom(
+        HEADERSIZE + SERVERPACKAGESIZE)
+    receivedBuffer = struct.unpack("!IIII", receiveData[HEADERSIZE:])
+    print("\tPacket contents: ", receivedBuffer[:])
+    num = receivedBuffer[0]
+    length = receivedBuffer[1]
+    udp_port = receivedBuffer[2]
+    secret_a = receivedBuffer[3]
+    print(f"\t\tnum: {num} \
+          \n\t\tlen: {length} \
+          \n\t\tudp_port: {udp_port} \
+          \n\t\tsecretA: {secret_a}")
 
-    # close the socket
+    # pass integers to stage b
+    p1_stage_b(receivedBuffer, socket_udp)
     socket_udp.close()
 
-def p1StageB(receivedBuffer, socket_udp): 
+
+def p1_stage_b(receivedBuffer, socket_udp):
+    print("\tSTAGE a complete.\n\tStarting STAGE b...\n")
     num = receivedBuffer[0]
     len = receivedBuffer[1]
     udp_port = receivedBuffer[2]
     secret = receivedBuffer[3]
     i = 0
-    print("Sending", num, "packets for Stage B...")
-    while i < num :
-        #create the payload by creating a package that has the first four bytes as the id and then an empty for the rest of the payload
+    while i < num:
+        # create the payload by creating a package that has the first four
+        # bytes as the id and then an empty for the rest of the payload
         payload = bytearray(len + 4)
         int_bytes = struct.pack('!I', i)
         payload[0:4] = int_bytes[0:4]
-        #add the header to the payload
-        sendData = createHeaderandPackagePayload(payload, secret, STEP1, DIGITS)
-        #send data to server
+        print(f"\tpayload {i}: {payload}\n")
+
+        # add the header to the payload
+        sendData = package_header_and_payload(
+            payload, secret, STEP1, DIGITS)
+
+        # send data to server
+        print(
+            f"\tSending UDP packet to {SERVER_ADDRESS} on port {udp_port}...")
         socket_udp.sendto(sendData, (SERVER_ADDRESS, udp_port))
-        receiveData, server_address = socket_udp.recvfrom(HEADERSIZE + SERVERACKSIZE)
+
+        # receive data from server
+        print(f"\tReceiving packet from {SERVER_ADDRESS}... ")
+        receiveData, server_address = socket_udp.recvfrom(
+            HEADERSIZE + SERVERACKSIZE)
         receivedBuffer = struct.unpack("!I", receiveData[HEADERSIZE:])
         if receivedBuffer[0] == i:
             i += 1
-    print("Sent all", num, "packets for Stage B.")
 
-    recData, recAddr = socket_udp.recvfrom(HEADERSIZE + 8)
+    recData = socket_udp.recvfrom(HEADERSIZE + 8)
     recBuffer = struct.unpack("!II", recData[HEADERSIZE:])
     tcp_port = recBuffer[0]
     secretB = recBuffer[1]
 
 
-
-
-def createUDPConnection():
+def create_udp_socket():
+    """"Creates UDP socket and gets addr info."""
     SERVERADDRESS = socket.gethostbyname('attu2.cs.washington.edu')
     socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #socket_udp.connect((SERVERADDRESS, PORT))
+    # socket_udp.connect((SERVERADDRESS, PORT))
     return socket_udp
 
-# Need to fill 12 bytes of data before the message as the header
-# First 4 bytes is the len of the message
-def createHeaderandPackagePayload(payload, secret, step, digits): 
-    if len(payload) % 4 == 0 :
-        payloadLen = len(payload)
-    else : 
-        payloadLen = (4 - len(payload) % 4) + len(payload)
-    header = struct.pack("!IIHH", len(payload), secret, step, digits)
-    return header + payload.ljust(payloadLen, b'\0')
+
+def package_header_and_payload(payload, secret, step, digits):
+    """Creates the 12-byte header and padded payload packet."""
+    payload_len = pad_length(payload)
+    header = struct.pack("!IIHH", payload_len, secret, step, digits)
+    return header + payload.ljust(payload_len, b'\0')
+
+
+def pad_length(payload):
+    """Computes padding needed for payload to be 4-byte aligned."""
+    if len(payload) % 4 == 0:
+        payload_len = len(payload)
+    else:
+        payload_len = (4 - len(payload) % 4) + len(payload)
+    return payload_len
+
 
 if __name__ == '__main__':
     main()
