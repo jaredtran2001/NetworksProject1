9d716ed3961884e52537305adab32121f6aa6a23
elulu3 <elena.wu2000@gmail.com>
2023-04-16 18:51:27 -0700

    p2 stage c and d - tentative

--- part2/Part2.py
+++ part2/Part2.py
@@ -1,4 +1,4 @@
-#from part1.Part1 import *
+from part1.Part1 import *
 import socket
 import struct
 import threading
@@ -75,10 +75,9 @@ def p2_stage_b(client_addr, udp_port, num, len, secretA):
             print("\tIncorrect header: ", header_data)
             return
         # get packet_id
-        client_payload = struct.unpack('!I', client_data[HEADERSIZE:HEADERSIZE + 4])
-        # randomly decide whether to send ack
+        client_payload = struct.unpack('!II', client_data[HEADERSIZE:HEADERSIZE + 8])
+        # randomly decide whether to send ack, respond with incorrect packet_id if not ack
         if random.random() < 0.5:
-            #TODO: NEED TO CHECK PAYLOAD SEND FROM CLIENT, APPEARS TO BE IN TUPLE FORM?
             ack_packet = struct.pack('!IIHHI', 4, secretA, STEP2, DIGITS, client_payload[0])
             num_recv += 1
         else:
@@ -93,6 +92,53 @@ def p2_stage_b(client_addr, udp_port, num, len, secretA):
     sock_b.sendto(payload, client_addr)
     sock_b.close()
     print("\tSTAGE b complete.")
+    p2_stage_c(tcp_port, secretB)
+
+def p2_stage_c(tcp_port, secretB):
+    print("\tStarting STAGE c...")
+    sock_c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_c.bind((SERVER_ADDRESS, tcp_port))
+    sock_c.listen()
+    client_conn, client_addr = sock_c.accept()
+    num2 = random.randint(1, 50)
+    len2 = random.randint(1, 50)
+    secretC = random.randint(1, 100)
+    char_c = 'c'
+    tcp_payload = struct.pack('!IIIc', num2, len2, secretC, char_c.encode('UTF-8'))
+    tcp_packet = package_header_and_payload(tcp_payload, secretB, STEP2, DIGITS)
+    sock_c.sendto(tcp_packet, client_addr)
+    print("\tSTAGE c complete.")
+    p2_stage_d(client_conn, num2, secretC, len2, client_addr)
+    sock_c.close()
+
+def p2_stage_d(client_conn, num2, secretC, len2, client_addr):
+    print("\tStarting STAGE d...")
+    if len2 % 4 == 0:
+        byte_aligned_len = len2
+    else:
+        byte_aligned_len = (4 - len2 % 4) + len2
+    
+    num_recv = 0
+    while num_recv < num2:
+        client_data = client_conn.recv(BUFFERSIZE)
+        header_data = client_data[:HEADERSIZE]
+        header_data = struct.unpack('!IIHH', header_data)
+        # verify header data
+        if header_data != (byte_aligned_len, secretC, STEP1, DIGITS):
+            print("\tIncorrect header: ", header_data)
+            return
+        # verify payload data that all are of the character c
+        client_payload = client_data[HEADERSIZE:HEADERSIZE + len2]
+        if not all(i == 'c' for i in client_payload):
+            print("\tIncorrect payload: ", client_payload)
+            return
+        num_recv += 1
+
+    # step d2
+    secretD = random.randint(1, 100)
+    d_payload = struct.pack('!IIHHI', 4, secretD, STEP2, DIGITS, secretD)
+    client_conn.sendto(d_payload, client_addr)
+    print("\tSTAGE d complete.")
 
 
 if __name__ == '__main__':
